70a1baccb2b6db90ace1af8b80abef330ed56a3a
Emile Belanger <emile.belanger@gmail.com>
2019-05-20 20:41:23 +0100

    Added Zandronum

--- androidcore/src/main/java/com/opentouchgaming/androidcore/GameEngine.java
+++ androidcore/src/main/java/com/opentouchgaming/androidcore/GameEngine.java
@@ -27,6 +27,7 @@ public class GameEngine
         YQUAKE2,
         IOQUAKE3,
         HEXEN2,
+        ZANDRONUM,
     }
 
     public GameEngine(Engine engine, int uiGroup, String title, String name, String directory, String[] versions, String[][] loadLibs, String args, ActionInputDefinition gamepadDefiniton, int iconRes, int color, Class opCls)
