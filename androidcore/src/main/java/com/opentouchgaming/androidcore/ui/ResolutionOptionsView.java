4b277c05ef00c28101d3f9ab8423745c35b3ee54
Emile Belanger <emile.belanger@gmail.com>
2022-02-20 19:19:30 +0000

    Allow engine specific Resolutions for ResolutionOptionsView. Added hide/show also

--- androidcore/src/main/java/com/opentouchgaming/androidcore/ui/ResolutionOptionsView.java
+++ androidcore/src/main/java/com/opentouchgaming/androidcore/ui/ResolutionOptionsView.java
@@ -17,21 +17,22 @@ import java.util.List;
 
 public class ResolutionOptionsView
 {
-
-    static List<ResolutionOptions> resolutionList = new ArrayList<>();
+    static List<ResolutionOptions> resolutionListDefault = new ArrayList<>();
 
     static
     {
-        resolutionList.add(new ResolutionOptions("Screen (100%)", ResolutionType.FULL, "$W", "$H"));
-        resolutionList.add(new ResolutionOptions("Screen / 2 (50%)", ResolutionType.FULL_0_5, "$W2", "$H2"));
-        resolutionList.add(new ResolutionOptions("Screen / 3 (33%)", ResolutionType.FULL_0_3, "$W3", "$H3"));
-        resolutionList.add(new ResolutionOptions("Screen / 4 (25%)", ResolutionType.FULL_0_25, "$W4", "$H4"));
-        resolutionList.add(new ResolutionOptions("568 x 320 (16:9)", ResolutionType.SET, "568", "320"));
-        resolutionList.add(new ResolutionOptions("850 x 480 (16:9)", ResolutionType.SET, "850", "480"));
-        resolutionList.add(new ResolutionOptions("769 x 480 (16:10)", ResolutionType.SET, "769", "480"));
-        resolutionList.add(new ResolutionOptions("1280 x 675 (16:9)", ResolutionType.SET, "1280", "675"));
-        resolutionList.add(new ResolutionOptions("Custom", ResolutionType.CUSTOM, "0", "0"));
+        resolutionListDefault.add(new ResolutionOptions("Screen (100%)", ResolutionType.FULL, "$W", "$H"));
+        resolutionListDefault.add(new ResolutionOptions("Screen / 2 (50%)", ResolutionType.FULL_0_5, "$W2", "$H2"));
+        resolutionListDefault.add(new ResolutionOptions("Screen / 3 (33%)", ResolutionType.FULL_0_3, "$W3", "$H3"));
+        resolutionListDefault.add(new ResolutionOptions("Screen / 4 (25%)", ResolutionType.FULL_0_25, "$W4", "$H4"));
+        resolutionListDefault.add(new ResolutionOptions("568 x 320 (16:9)", ResolutionType.SET, "568", "320"));
+        resolutionListDefault.add(new ResolutionOptions("850 x 480 (16:9)", ResolutionType.SET, "850", "480"));
+        resolutionListDefault.add(new ResolutionOptions("769 x 480 (16:10)", ResolutionType.SET, "769", "480"));
+        resolutionListDefault.add(new ResolutionOptions("1280 x 675 (16:9)", ResolutionType.SET, "1280", "675"));
+        resolutionListDefault.add(new ResolutionOptions("Custom", ResolutionType.CUSTOM, "0", "0"));
     }
+
+    List<ResolutionOptions> resolutionList;
 
     String prefix;
     EditText widthEdit;
@@ -39,13 +40,24 @@ public class ResolutionOptionsView
     Spinner resSpinner;
     LinearLayout topLayout;
 
+    public ResolutionOptionsView(Context context, View view, String prefix, List<ResolutionOptions> resolutions)
+    {
+        Init(context, view, prefix, resolutions);
+    }
+
     public ResolutionOptionsView(Context context, View view, String prefix)
     {
-        this.prefix = prefix;
+        Init(context, view, prefix, resolutionListDefault);
+    }
 
-        resSpinner = view.findViewById(com.opentouchgaming.androidcore.R.id.resolution_spinner);
-        widthEdit = view.findViewById(com.opentouchgaming.androidcore.R.id.resolution_width_editText);
-        heightEdit = view.findViewById(com.opentouchgaming.androidcore.R.id.resolution_height_editText);
+    public void Init(Context context, View view, String prefix, List<ResolutionOptions> resolutions)
+    {
+        this.prefix = prefix;
+        this.resolutionList = resolutions;
+
+        resSpinner = view.findViewById(R.id.resolution_spinner);
+        widthEdit = view.findViewById(R.id.resolution_width_editText);
+        heightEdit = view.findViewById(R.id.resolution_height_editText);
         topLayout = view.findViewById(R.id.resolution_layout);
 
         ArrayAdapter<ResolutionOptions> dataAdapter = new ArrayAdapter<>(context, android.R.layout.simple_spinner_item, resolutionList);
@@ -57,7 +69,7 @@ public class ResolutionOptionsView
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
             {
                 AppSettings.setIntOption(context, prefix + "_resolution_spinner", position);
-                ResolutionOptions option = getResOption(prefix);
+                ResolutionOptions option = getResOption(prefix, resolutionList);
                 widthEdit.setText("" + option.w);
                 heightEdit.setText("" + option.h);
                 if (option.type == ResolutionType.CUSTOM)
@@ -79,16 +91,19 @@ public class ResolutionOptionsView
         });
 
         int selected = AppSettings.getIntOption(context, prefix + "_resolution_spinner", 0);
-        resSpinner.setSelection(selected);
+        if(selected < resolutionList.size())
+            resSpinner.setSelection(selected);
+        else
+            resSpinner.setSelection(0);
     }
 
     public void setEnabled(boolean enabled)
     {
-        if(enabled)
+        if (enabled)
         {
             resSpinner.setEnabled(true);
 
-            ResolutionOptions option = getResOption(prefix);
+            ResolutionOptions option = getResOption(prefix, resolutionList);
             if (option.type == ResolutionType.CUSTOM)
             {
                 widthEdit.setEnabled(true);
@@ -108,7 +123,19 @@ public class ResolutionOptionsView
         }
     }
 
-    public static ResolutionOptions getResOption(String prefix)
+    public void setHidden(boolean hidden)
+    {
+        if (hidden)
+        {
+            topLayout.setVisibility(View.GONE);
+        }
+        else
+        {
+            topLayout.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public static ResolutionOptions getResOption(String prefix,  List<ResolutionOptions> resolutionList)
     {
         int selected = AppSettings.getIntOption(AppInfo.getContext(), prefix + "_resolution_spinner", 0);
         ResolutionOptions option = resolutionList.get(selected);
@@ -120,6 +147,11 @@ public class ResolutionOptionsView
         return option;
     }
 
+    public static ResolutionOptions getResOption(String prefix)
+    {
+        return getResOption(prefix, resolutionListDefault);
+    }
+
     public void save()
     {
         if (getResOption(prefix).type == ResolutionType.CUSTOM)
@@ -129,7 +161,7 @@ public class ResolutionOptionsView
         }
     }
 
-    enum ResolutionType
+    public enum ResolutionType
     {
         FULL, FULL_0_5, FULL_0_3, FULL_0_25, SET, CUSTOM
     }
@@ -140,7 +172,8 @@ public class ResolutionOptionsView
         public String h;
         String title;
         ResolutionType type;
-        ResolutionOptions(String title, ResolutionType t, String w, String h)
+
+        public ResolutionOptions(String title, ResolutionType t, String w, String h)
         {
             this.title = title;
             this.type = t;
